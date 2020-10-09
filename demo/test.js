06fad2c3a3aa3e6c5e2a500d80c5f6c5fed7676d
rys-32 <>
2020-10-09 20:44:37 +0800

    nm

---
+++ demo/test.js
@@ -0,0 +1,8 @@
+/**
+ * @ignore
+ * @Description nothing
+ * @author Wang Bo (ralwayne@163.com)
+ * @date 2020/10/9 8:24 PM
+ */
+const shell = require('shelljs')
+shell.exec('git clone git@github.com:FlashHand/rig.git')
